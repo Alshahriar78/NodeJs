6cf96ff0d75d342afb9ea3028190de44bebaa497
Alshahriar78 <alshahriar78@gmail.com>
2025-02-26 16:49:16 +0600

    file system Module

---
+++ coreModules/fsModule.js
@@ -0,0 +1,6 @@
+let fs = require('node:fs');
+let writeData= fs.writeFileSync('note.txt',"Don't worry. you are bacame Updated version.Everthing will be ok In sha Allah.",'utf8');
+let updateWrite = fs.appendFileSync('note.txt','Allah is Enough for you. Be Patient');
+let data = fs.readFileSync('note.txt','utf8');
+let del = fs.unlinkSync('index.html')
+console.log();
