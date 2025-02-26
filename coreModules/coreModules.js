49400de5a4602362b11e198916c8844ddc785ad8
Alshahriar78 <alshahriar78@gmail.com>
2025-02-26 14:10:29 +0600

    file system module

--- coreModules/coreModules.js
+++ coreModules/coreModules.js
@@ -1,4 +1,15 @@
 const path = require('node:path');
+const os = require('node:os');
+const fs = require('node:fs')
+
 const myPath= 'E:/AL_Shahoriar/New folder/NodeJs/coreModules/coreModules.js';
 
-console.log(path.parse(myPath));
+// console.log(path.parse(myPath));
+fs.readFile('note.txt',(err,data)=>{
+    console.log(data.toString());
+})
+console.log(os.homedir())
+console.log('Hello')
+
+
+// Events -> event driven 
