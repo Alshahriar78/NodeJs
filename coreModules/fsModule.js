06b89434830f578d1ccc2ad816378cadb0f79fe2
Alshahriar78 <alshahriar78@gmail.com>
2025-02-26 17:50:33 +0600

    asyncro....

--- coreModules/fsModule.js
+++ coreModules/fsModule.js
@@ -1,6 +1,10 @@
 let fs = require('node:fs');
 let writeData= fs.writeFileSync('note.txt',"Don't worry. you are bacame Updated version.Everthing will be ok In sha Allah.",'utf8');
 let updateWrite = fs.appendFileSync('note.txt','Allah is Enough for you. Be Patient');
-let data = fs.readFileSync('note.txt','utf8');
+let data = fs.readFileSync('note.txt');
+fs.readFile('note.txt',(err,data)=>{
+    console.log(data.toString());
+})
 
-console.log();
+console.log(data.toString());
+
